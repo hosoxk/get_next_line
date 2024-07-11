3384438809f9193c11544b864983ed47898e6570
Yoni De Rudder <yoni_de_rudder@hotmail.com>
2024-07-11 20:19:33 +0200

    fixed some norminette errors

--- main.c
+++ main.c
@@ -1,30 +1,40 @@
-#include <stdio.h>
-#include <fcntl.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yde-rudd <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/11 20:14:54 by yde-rudd          #+#    #+#             */
+/*   Updated: 2024/07/11 20:15:37 by yde-rudd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
 
-int main(void)
+int	main(void)
 {
-    int     fd;
-    char    *line;
-    int     count;
+	int		fd;
+	char	*line;
+	int		count;
 
-    count = 0;
-    fd = open("example.txt", O_RDONLY);
-    if (fd == -1)
-    {
-        printf("Error opening file");
-        return (1);
-    }
-    while (1)
-    {
-        line = get_next_line(fd);
-        if (line == NULL)
-            break;
-        count++;
-        printf("[%d]:%s\n, count, line"); //   count is to show you the line numbers
-        free(line);
-        line = NULL;
-    }
-    close(fd);
-    return (0);
+	count = 0;
+	fd = open("example.txt", O_RDONLY);
+	if (fd == -1)
+	{
+		printf("Error opening file");
+		return (1);
+	}
+	while (line = get_next_line(fd))
+	{
+		count++;
+		// count is to show you the line numbers
+		printf("[%d]:%s\n, count, line");
+		free(line);
+		line = NULL;
+	}
+	close(fd);
+	return (0);
 }
